10e8ecb913643cb8e0a5beffa0af628de45dd5be
ohbarye <over.rye@gmail.com>
2016-12-15 02:11:43 +0900

    Implement bony framework of KPT bot

---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const Botkit = require('botkit');
+
+const slackBotToken = process.env.SLACK_BOT_TOKEN;
+
+if (!slackBotToken) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const controller = Botkit.slackbot({
+  debug: !!process.env.DEBUG
+});
+
+const bot = controller.spawn({
+  token: slackBotToken
+})
+
+bot.startRTM(function(err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+});
+
+/*
+   This KPI bot waits for you to call him. Here is sample usage.
+
+   Say: @bot-name 2016/11/01 2016/11/31
+
+   The bot gathers KPTs you posted from 2016/11/01 and 2016/11/31
+   from history of a channel you called the bot.
+
+   ```
+   K
+   - Started daily meeting
+   - Find a release blocker earlier
+   P
+   - Cannot help each other...
+   T
+   - How about going lunch? :smile:
+   ```
+ */
+controller.hears("(.+)",["direct_message","direct_mention","mention"], (bot, message) => {
+  const [from, to] = message.match[0].split(' ');
+
+  let params = {
+    token: slackBotToken,
+    channel: message.channel,
+    oldest: (new Date(from) / 1000),
+    latest: (new Date(to) / 1000),
+    count: 1000,
+  }
+
+  bot.api.callAPI('channels.history', params, (err, json) => {
+    if (err) {
+      throw new Error(`Slack API returns an error. error code: ${err}`);
+    }
+
+    let result = {
+      k: [],
+      p: [],
+      t: [],
+    }
+
+    for (const message of json.messages) {
+      bot.botkit.log(JSON.stringify(message));
+      const matched = message.text.match(/^([KkPpTt])\s+(.+)/);
+      if (matched) {
+        result[matched[1].toLowerCase()].push({
+          content: matched[2]
+        });
+      }
+    }
+
+    const summary = `
+K
+${result.k.map((k) => `- ${k.content}` ).join('\n')}
+P
+${result.p.map((p) => `- ${p.content}` ).join('\n')}
+T
+${result.t.map((t) => `- ${t.content}` ).join('\n')}
+`
+
+    bot.reply(message, summary);
+  });
+});
+
