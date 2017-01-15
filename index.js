4ccef90daa625a56e6ecc5680b5802b50c30fcd5
ohbarye <over.rye@gmail.com>
2017-01-15 22:43:10 +0900

    Extract method to create parameters

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ if (!slackBotToken) {
   process.exit(1);
 }
 
+const commonParams = {
+  token: slackBotToken,
+};
+
 const controller = Botkit.slackbot({
   debug: !!process.env.DEBUG
 });
@@ -45,33 +49,20 @@ bot.startRTM(function(err, bot, payload) {
    ```
  */
 controller.hears("^summary (.+)",["direct_message","direct_mention","mention"], (bot, message) => {
-  const [from_date, to_date] = message.match[1].split(' ');
-
-  let params = {
-    token: slackBotToken,
-    channel: message.channel,
-    oldest: (new Date(from_date) / 1000),
-    count: 1000,
-  }
-
-  // `latest` is optional parameter, and its default value is `now`.
-  if (to_date) {
-    params.latest = new Date(to_date) / 1000
-  }
-
   let users;
 
   const fetchUserListDone = (err, res) => {
-    checkError(err)
-    users = res.members
+    checkError(err);
+    users = res.members;
+
     // https://api.slack.com/methods/channels.history
-    bot.api.callAPI('channels.history', params, postSummary);
-  }
+    bot.api.callAPI('channels.history', paramsToFetchChannelHistory(message, users), postSummary);
+  };
 
   const postSummary = (err, res) => {
-    checkError(err)
+    checkError(err);
 
-    let result = { K: [], P: [], T: [] }
+    let result = { K: [], P: [], T: [] };
 
     for (const message of res.messages) {
       const matched = message.text.match(/^([KkPpTt])\s+(.+)/);
@@ -86,10 +77,10 @@ controller.hears("^summary (.+)",["direct_message","direct_mention","mention"], 
     }
 
     bot.reply(message, createSummary(result, users));
-  }
+  };
 
   // https://api.slack.com/methods/users.list
-  bot.api.callAPI('users.list', params, fetchUserListDone)
+  bot.api.callAPI('users.list', commonParams, fetchUserListDone)
 });
 
 /*
@@ -110,13 +101,13 @@ const checkError = (err) => {
   if (err) {
     throw new Error(`Slack API returns an error. error code: ${err}`);
   }
-}
+};
 
 const createSummary = (result, users) => {
   return ['K', 'P', 'T'].map((section) => {
     return `## ${section}\n\n${createSectionSummary(result[section], users)}\n`
   }).join('\n')
-}
+};
 
 const createSectionSummary = (elements, users) => {
   return elements.map(e => {
@@ -125,4 +116,20 @@ const createSectionSummary = (elements, users) => {
 
     return `- ${e.content} by ${username} ${reactions}`;
   }).join('\n')
-}
+};
+
+const paramsToFetchChannelHistory = (message) => {
+  const [from_date, to_date] = message.match[1].split(' ');
+
+  let params = Object.assign(commonParams, {
+    channel: message.channel,
+    oldest: (new Date(from_date) / 1000),
+    count: 1000,
+  });
+
+  // `latest` is optional parameter, and its default value is `now`.
+  if (to_date) {
+    params = Object.assign(params, {latest: new Date(to_date) / 1000});
+  }
+  return params;
+};
